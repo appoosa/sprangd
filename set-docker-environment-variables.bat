0eb20c686f56aee69b85b3c371c85299f7b7fc90
Girish Gangadharan <girish.gangadharan@projekt202.com>
2016-02-16 15:54:26 -0600

    Updated code to dynamically pull in the username and the ip address

--- set-docker-environment-variables.bat
+++ set-docker-environment-variables.bat
@@ -1,4 +1,14 @@
+:: This will work only for Windows users
+
+:: Grab the IP address as used by the docker machine and assign it to IP_ADDRESS variable.
+:: Docker Host has to be running for this to work, of course.
+FOR /f %%i in ('docker-machine ip default') DO SET IP_ADDRESS=%%i
 SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.100:2376
-SET DOCKER_CERT_PATH=C:\Users\Girish.Gangadharan\.docker\machine\machines\default
+
+:: Set the DOCKER_HOST based on the default IP address
+SET DOCKER_HOST=tcp://%IP_ADDRESS%:2376
+
+::Set the path based on the username of the logged in user - %USERNAME%.
+SET DOCKER_CERT_PATH=C:\Users\%USERNAME%\.docker\machine\machines\default
+
 SET DOCKER_MACHINE_NAME=default
